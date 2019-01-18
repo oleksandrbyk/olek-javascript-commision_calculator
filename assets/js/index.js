ec71d4afc0419baf2e6aa54d61b3ef1b398ff98d
oleksandrbyk <obyk320@gmail.com>
2019-01-18 16:37:31 +0100

    completed  commission calculator

--- assets/js/index.js
+++ assets/js/index.js
@@ -33,19 +33,42 @@ $(document).ready(function() {
     sliders = sliderIds.map((sliderName) => new Slider('#' + sliderName, {}));
   }
 
+  const updatePaidDigs = () => {
+    let a = sliders[0].getValue();
+    let b = sliders[1].getValue();
+    let c = 2.5;
+    let d;
+    let e = sliders[2].getValue();
+    let f = sliders[3].getValue();
+    let g = sliders[4].getValue();
+    let h = sliders[5].getValue();
+    let te = e * 12;
+    let tf = f / 100 * a * b * c;
+    let tg = g * a;
+    d = te + tf + tg + h;
+    d = d > 6000? 6000: d;
+    d = d.toFixed(0);
+    $("#paid-to-giddy-digs")[0].innerHTML = "Paid to Giddy Digs: $"+ addComma(d);
+  }
+
   generateSliders();
 
   sliders.forEach((slider, idx) => {
     slider.on('slide', (value)=> {
+      slider.setValue(value);
       $("#" + sliderIds[idx] + "-description")[0].innerHTML = description[idx].unit + addComma(value) + description[idx].suffix;
+
       // lock when current value reach maximum value
-      if(slider.options.max <= value) {
-        slider.sliderElem.previousElementSibling.classList.remove("unlock");
-        slider.sliderElem.previousElementSibling.classList.add("lock");
-      } else {
-        slider.sliderElem.previousElementSibling.classList.remove("lock");
-        slider.sliderElem.previousElementSibling.classList.add("unlock");
+      if(idx > 1) {
+        if(slider.options.max <= value) {
+          slider.sliderElem.previousElementSibling.classList.remove("unlock");
+          slider.sliderElem.previousElementSibling.classList.add("lock");
+        } else {
+          slider.sliderElem.previousElementSibling.classList.remove("lock");
+          slider.sliderElem.previousElementSibling.classList.add("unlock");
+        }
       }
+      updatePaidDigs();
     });
   });
 
